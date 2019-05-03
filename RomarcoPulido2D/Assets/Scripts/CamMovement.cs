c0f3da8a9929f7038344f216684b97b6757d927e
romarco1 <romarco98@gmail.com>
2019-05-02 22:23:35 -0500

    primer clase 2D

---
+++ RomarcoPulido2D/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMovement : MonoBehaviour{
+    public Transform followTarget;
+    public float followSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (followTarget) {
+            Vector3 temp = Vector3.MoveTowards (transform.position, followTarget.position, followSpeed * Time.deltaTime);
+            temp.z = transform.position.z;
+            transform.position = temp;
+             
+        } 
+    }
+}
---
+++ RomarcoPulido2D/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 horMove = Input.GetAxis ("Horizontal") * Vector3.right;
+        Vector3 verMove = Input.GetAxis ("Vertical") * Vector3.up;
+
+        transform.Translate ((horMove + verMove).normalized * moveSpeed * Time.deltaTime);
+
+    }
+}
