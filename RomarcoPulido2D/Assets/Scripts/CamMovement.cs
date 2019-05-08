c41e16af24231b2945810f167222ebe94d439284
romarco1 <romarco98@gmail.com>
2019-05-07 22:22:44 -0500

    limits

--- RomarcoPulido2D/Assets/Scripts/CamMovement.cs
+++ RomarcoPulido2D/Assets/Scripts/CamMovement.cs
@@ -5,19 +5,22 @@ using UnityEngine;
 public class CamMovement : MonoBehaviour{
     public Transform followTarget;
     public float followSpeed;
+    Vector2 camUnitDimensions = new Vector2 (8.9f, 5.0f);
+    Vector2 limits;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+        camUnitDimensions = new Vector2 (Camera.main.orthographicSize * 16 / 9, Camera.main.orthographicSize);
+        limits = new Vector2 (10.0f, 7.0f) - camUnitDimensions;
         
     }
 
     // Update is called once per frame
     void Update(){
         if (followTarget) {
-            Vector3 temp = Vector3.MoveTowards (transform.position, followTarget.position, followSpeed * Time.deltaTime);
+            /*Vector3 temp = Vector3.MoveTowards (transform.position, followTarget.position, followSpeed * Time.deltaTime);
             temp.z = transform.position.z;
-            transform.position = temp;
-             
+            transform.position = temp;*/
+            Vector3 direction = (followTarget.position - transform.position).normalized;
         } 
     }
 }
--- RomarcoPulido2D/Assets/Scripts/PlayerMovement.cs
+++ RomarcoPulido2D/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,53 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 15;
+    public Vector2 limits = new Vector2 (5, 3.5f);
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start(){
+
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        Vector3 horMove = Input.GetAxis ("Horizontal") * Vector3.right;
-        Vector3 verMove = Input.GetAxis ("Vertical") * Vector3.up;
+    void Update(){
+        
+        //Get current Horizontal Movement
+        float horDirection = Input.GetAxisRaw ("Horizontal");
+;        Vector3 horMove = horDirection * Vector3.right;
+        
+        /*if (transform.position.x > -limits.x || transform.position.x < limits.x) {
+            horMove *= 0;
+            int limit = (int)transform.position.x;
+            Vector3 temp = transform.position;
+            temp.x = limit;
+            transform.position = temp;
+        }*/
+        //Get current Vertical Movement
+        float verDirection = Input.GetAxisRaw ("Vertical");
+        Vector3 verMove = verDirection * Vector3.up;
 
+        Vector3 temp = transform.position;
         transform.Translate ((horMove + verMove).normalized * moveSpeed * Time.deltaTime);
+        temp.x = Mathf.Clamp (transform.position.x, -limits.x, limits.x);
+        temp.y = Mathf.Clamp (transform.position.y, -limits.x, limits.y);
+        transform.position = temp;
+
+        /*if ((transform.position.x < -limits.x || transform.position.x > limits.x) ||
+            (transform.position.y < -limits.y || transform.position.y > limits.y)) {
+            transform.position = Temp;
+        }*/
+
+    }
+
+    void OnTriggerEnter2D (Collider2D other){
+        Debug.Log ("colision");
+
+    }
+
+    void OnDrawGizmos (){
+        Gizmos.DrawSphere (transform.position, 0.15f);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube (Vector3.zero, limits);
 
     }
 }
