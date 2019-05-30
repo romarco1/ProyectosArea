79128a1d54822a77e01bae3ba1018fa23fc5ec5b
romarco1 <romarco98@gmail.com>
2019-05-29 19:31:18 -0500

    trabajos

---
+++ RomarcoPulido2D/Assets/PlatformerExample/Scripts/PlatformMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour {
+
+    public float gravity = 9;
+    public float horizontalSpeed = 15;
+
+    float verticalSpeed;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        transform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * horizontalSpeed * Time.deltaTime);
+        verticalSpeed += gravity * Time.deltaTime;
+        transform.Translate (Vector3.down * verticalSpeed * Time.deltaTime);
+    }
+}
