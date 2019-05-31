7c828d8239ffe932055227bc2d749e897722aca3
romarco1 <romarco98@gmail.com>
2019-05-30 22:19:54 -0500

    raycast en plataformas

--- RomarcoPulido2D/Assets/PlatformerExample/Scripts/PlatformMovement.cs
+++ RomarcoPulido2D/Assets/PlatformerExample/Scripts/PlatformMovement.cs
@@ -6,8 +6,15 @@ public class PlatformMovement : MonoBehaviour {
 
     public float gravity = 9;
     public float horizontalSpeed = 15;
+    public Collider2D playerCollider;
 
+    public float width { get { return playerCollider.bounds.size.x * transform.localScale.x; } }
+    public float height { get { return playerCollider.bounds.size.y * transform.localScale.y; } }
+    Vector3 leftCorner { get { return transform.position - (Vector3.right * width / 2); } }
+    Vector3 rightCorner { get { return transform.position + (Vector3.right * width / 2); } }
     float verticalSpeed;
+    bool grounded;
+    float detectionDistance = 0.2f;
 
     // Start is called before the first frame update
     void Start () {
@@ -17,8 +24,46 @@ public class PlatformMovement : MonoBehaviour {
     // Update is called once per frame
     void Update () {
 
-        transform.Translate (Vector3.right * Input.GetAxis ("Horizontal") * horizontalSpeed * Time.deltaTime);
-        verticalSpeed += gravity * Time.deltaTime;
-        transform.Translate (Vector3.down * verticalSpeed * Time.deltaTime);
+        RaycastHit2D lefthit = Physics2D.Raycast (leftCorner, Vector3.down * detectionDistance);
+        RaycastHit2D righthit=Physics2D.Raycast (rightCorner, Vector3.down * detectionDistance);
+
+        if (lefthit || righthit) {
+            grounded = true;
+            
+        } else {
+            grounded = false;
+        }
+
+        Vector3 horizontalMovement = Vector3.right * Input.GetAxis ("Horizontal") * horizontalSpeed * Time.deltaTime;
+        if (!grounded) {
+            verticalSpeed -= gravity * Time.deltaTime;
+           
+        }
+        Vector3 verticalMovement = Vector3.up * verticalSpeed;
+        transform.Translate (horizontalMovement + verticalMovement);
+    }
+
+    /*void OnTriggerEnter2D (Collider2D other){
+        if (other.CompareTag ("Platform")) {
+            verticalSpeed = 0;
+            grounded = true;
+
+            float halfHeight = other.bounds.size.y / 2;
+            float positionDistance = Mathf.Abs (transform.position.y - other.transform.position.y);
+            float pushDelta = halfHeight - positionDistance;
+
+            transform.Translate (Vector3.up * pushDelta);
+        }
+        
+
+    }
+*/
+    void OnDrawGizmos (){
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay (leftCorner, Vector3.down);
+        Gizmos.DrawRay (rightCorner, Vector3.down);
+
+        Gizmos.DrawSphere (transform.position-(Vector3.right*width/2), 0.05f);
+        Gizmos.DrawSphere (transform.position + (Vector3.right * width / 2), 0.05f);
     }
 }
